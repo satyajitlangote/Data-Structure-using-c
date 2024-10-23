0079d4b246a90d6038bc96a4406ea6a7ba765a89
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-10-23 19:32:13 +0530

    Stack array in dsc  program

---
+++ Stackarray.c
@@ -0,0 +1,65 @@
+#include <stdio.h> 
+#include<conio.h>
+int stack[100],i,j,choice=0,n,top=-1; 
+void push(); 
+void pop(); 
+void show(); 
+void main () { 
+  printf("Name:-Satyajit Hanmant Langote\n Class:-BCA-II SEM-III\n Roll_No:-33\nSubject: Lab Course VI based on CC303(Data Structure using C)\nAssignment No:-7 & Name:-Stack Array\n");
+ 	printf("Enter the number of elements in the stack "); 
+ 	scanf("%d",&n); 
+ 	printf("***Stack operations using array***"); 
+ 	printf("\n-------------------------\n"); 
+ 	while(choice != 4) { 
+ 		printf("Chose one from the below options...\n"); 
+ 		printf("\n1.Push\n2.Pop\n3.Show\n4.Exit"); 
+ 		printf("\n Enter your choice \n"); 
+ 		scanf("%d",&choice); 
+ 		switch(choice) { 
+ 			case 1: {
+					push(); 
+ 					break; 
+ 			} 
+ 			case 2: { 
+					pop(); 
+ 					break; 
+ 			} 
+ 			case 3: { 
+ 					show(); 
+ 					break; 
+ 				} 
+ 			case 4: { 
+ 					printf("Exiting...."); 
+ 					break; 
+ 				} 
+ 			default: { 
+ 					printf("Please Enter valid choice "); 
+				 } 
+ 		}; 
+ 	} 
+} 
+void push () { 
+ 	int val; 
+ 	if (top == n ) 
+ 	printf("\n Overflow"); 
+ 	else { 
+ 		printf("Enter the value?"); 
+ 		scanf("%d",&val); 
+ 		top = top +1; 
+ 		stack[top] = val; 
+ 	} 
+}  
+void pop () { 
+	if(top == -1) 
+ 	printf("Underflow"); 
+ 	else 
+ 	top = top -1; 
+} 
+void show() { 
+ 	for (i=top;i>=0;i--) { 
+ 	printf("%d\n",stack[i]); 
+	} 
+ 	if(top == -1) { 
+ 		printf("Stack is empty"); 
+	} 
+}
